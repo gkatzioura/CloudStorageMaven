a3b5f5fe7dde88228e2c83853317e2bb9e01e911
gkatzioura <gkatzioura@gmail.com>
2019-01-12 19:55:35 +0000

    removing local tests

--- S3StorageWagon/src/test/java/com/gkatzioura/maven/cloud/s3/plugin/download/S3DownloadMojoTest.java
+++ S3StorageWagon/src/test/java/com/gkatzioura/maven/cloud/s3/plugin/download/S3DownloadMojoTest.java
@@ -9,25 +9,4 @@ import org.junit.Test;
 
 public class S3DownloadMojoTest {
 
-    @Test
-    public void testDownloadBucket() throws Exception {
-
-        List<String> files = new ArrayList<>();
-        files.add("a-key-to-download.txt");
-        files.add("25226_big.jpg");
-        files.add("738ypD45_400x400.jpg");
-
-        S3DownloadMojo s3DownloadMojo = new S3DownloadMojo("gkatzfiledownloads",files,"/Users/gkatzioura/Documents/s3filedownload/downloads");
-        s3DownloadMojo.execute();
-    }
-
-    @Test
-    public void testDownloadPrefix() throws Exception {
-        List<String> files = new ArrayList<>();
-        files.add("prefix");
-
-        S3DownloadMojo s3DownloadMojo = new S3DownloadMojo("gkatzfiledownloads",files,"/Users/gkatzioura/Documents/s3filedownload/downloads/onprefix");
-        s3DownloadMojo.execute();
-    }
-
 }
