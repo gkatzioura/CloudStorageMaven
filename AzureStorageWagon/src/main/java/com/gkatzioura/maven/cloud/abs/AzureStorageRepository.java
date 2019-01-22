0d4246cf20c0524211a4fb1af69229c0bc3569ee
ERt <ert.wroc@gmail.com>
2019-01-22 09:50:19 +0100

    Set content type for azure blob storage.
    Without content type set website hosting (see https://azure.microsoft.com/en-us/blog/azure-storage-static-web-hosting-public-preview/) doesn't work correct (browser download all files, including .html instead display it).

--- AzureStorageWagon/src/main/java/com/gkatzioura/maven/cloud/abs/AzureStorageRepository.java
+++ AzureStorageWagon/src/main/java/com/gkatzioura/maven/cloud/abs/AzureStorageRepository.java
@@ -120,6 +120,7 @@ public class AzureStorageRepository {
         try {
 
             CloudBlockBlob blob = blobContainer.getBlockBlobReference(destination);
+            blob.getProperties().setContentType(getContentType(file));
 
             try(InputStream inputStream = new TransferProgressFileInputStream(file,transferProgress)) {
                 blob.upload(inputStream,-1);
@@ -127,6 +128,29 @@ public class AzureStorageRepository {
         } catch (URISyntaxException |StorageException | IOException e) {
             LOGGER.log(Level.SEVERE,"Could not fetch cloud blob",e);
             throw new TransferFailedException(destination);
+        }
+    }
+
+    private String getContentType(File file) {
+        String name = file.getName().toLowerCase();
+        if (name.endsWith(".txt")) {
+            return "text/plain";
+        } else if (name.endsWith(".js")) {
+            return "text/javascript";
+        } else if (name.endsWith(".css")) {
+            return "text/css";
+        } else if (name.endsWith(".htm") || name.endsWith(".html")) {
+            return "text/html";
+        } else if (name.endsWith(".json")) {
+            return "application/json";
+        } else if (name.endsWith(".jpg") || name.endsWith(".jpeg")) {
+            return "image/jpeg";
+        } else if (name.endsWith(".png")) {
+            return "image/png";
+        } else if (name.endsWith(".gif")) {
+            return "image/gif";
+        } else {
+            return "application/octet-stream";
         }
     }
 
