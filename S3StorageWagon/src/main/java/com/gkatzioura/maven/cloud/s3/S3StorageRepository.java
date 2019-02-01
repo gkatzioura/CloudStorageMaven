b1c161fd3cb2f577b0f57348e83a547422b9e80b
gkatzioura <gkatzioura@gmail.com>
2019-02-01 20:24:16 +0000

    Changed to snapshot

--- S3StorageWagon/src/main/java/com/gkatzioura/maven/cloud/s3/S3StorageRepository.java
+++ S3StorageWagon/src/main/java/com/gkatzioura/maven/cloud/s3/S3StorageRepository.java
@@ -36,6 +36,7 @@ import org.apache.maven.wagon.authentication.AuthenticationInfo;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ListObjectsRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.ObjectMetadata;
@@ -61,11 +62,19 @@ public class S3StorageRepository {
     public S3StorageRepository(String bucket) {
         this.bucket = bucket;
         this.baseDirectory = "";
+        this.publicReadProperty = new PublicReadProperty(false);
+    }
+
+    public S3StorageRepository(String bucket, PublicReadProperty publicReadProperty) {
+        this.bucket = bucket;
+        this.baseDirectory = "";
+        this.publicReadProperty = publicReadProperty;
     }
 
     public S3StorageRepository(String bucket, String baseDirectory) {
         this.bucket = bucket;
         this.baseDirectory = baseDirectory;
+        this.publicReadProperty = new PublicReadProperty(false);
     }
 
     public S3StorageRepository(String bucket, String baseDirectory, PublicReadProperty publicReadProperty) {
@@ -146,6 +155,7 @@ public class S3StorageRepository {
         try {
             try(InputStream inputStream = new TransferProgressFileInputStream(file,transferProgress)) {
                 PutObjectRequest putObjectRequest = new PutObjectRequest(bucket,key,inputStream,new ObjectMetadata());
+                applyPublicRead(putObjectRequest);
                 amazonS3.putObject(putObjectRequest);
             }
         } catch (AmazonS3Exception | IOException e) {
@@ -182,6 +192,13 @@ public class S3StorageRepository {
         List<String> objects = new ArrayList<>();
         retrieveAllObjects(objectListing, objects);
         return objects;
+    }
+
+    private void applyPublicRead(PutObjectRequest putObjectRequest) {
+        if(publicReadProperty!=null && publicReadProperty.get()) {
+            LOGGER.info("Public read was set to true");
+            putObjectRequest.withCannedAcl(CannedAccessControlList.PublicRead);
+        }
     }
 
     private void retrieveAllObjects(ObjectListing objectListing, List<String> objects) {
