332073d7c939dbff8e552d6481aaf3a8ab3b85a2
gkatzioura <gkatzioura@gmail.com>
2019-03-02 22:57:58 +0000

    Changed the upload in order to obtain size

--- S3StorageWagon/src/main/java/com/gkatzioura/maven/cloud/s3/plugin/download/S3UploadMojo.java
+++ S3StorageWagon/src/main/java/com/gkatzioura/maven/cloud/s3/plugin/download/S3UploadMojo.java
@@ -17,6 +17,10 @@
 package com.gkatzioura.maven.cloud.s3.plugin.download;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +32,7 @@ import org.apache.maven.plugins.annotations.Parameter;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
 @Mojo(name = "s3-upload")
@@ -75,12 +80,22 @@ public class S3UploadMojo extends AbstractMojo {
             List<String> filesToUpload = findFilesToUpload(path);
 
             for(String fileToUpload: filesToUpload) {
-                PutObjectRequest putObjectRequest = new PutObjectRequest(bucket,generateKeyName(fileToUpload),new File(fileToUpload));
-                amazonS3.putObject(putObjectRequest);
+                keyUpload(amazonS3, generateKeyName(fileToUpload), new File(fileToUpload));
             }
         } else {
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucket,keyIfNull(),new File(path));
+            keyUpload(amazonS3, keyIfNull(), new File(path));
+        }
+    }
+
+    private void keyUpload(AmazonS3 amazonS3, String keyName, File file) throws MojoExecutionException {
+        try (InputStream inputStream = new FileInputStream(file)) {
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentLength(file.length());
+
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucket, keyName, inputStream, objectMetadata);
             amazonS3.putObject(putObjectRequest);
+        } catch (IOException e) {
+            throw new MojoExecutionException("Failed to upload mojo",e);
         }
     }
 
